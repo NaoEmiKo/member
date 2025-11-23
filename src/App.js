93fe5e371d88c5b34aaf53c3cfb04ffa257cbd64
NaoEmiKo <nsegks2023@gmail.com>
2025-11-23 23:44:49 +0900

    added components Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import Header from '../components/Header'
+
 function App() {
   return (
     <div>
       <h1>Hello Naoto!</h1>
       <p>これは僕のReactページです。</p>
       <p>Hello again</p>
+      <Header />
     </div>
   );
 }
